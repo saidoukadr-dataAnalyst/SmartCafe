59214083f81885d85bc0719fc28814e3ecc12bfe
saidoukadr-dataAnalyst <saidoukadr@gmail.com>
2026-06-07 03:02:07 +0100

    custom-icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.cafemanager.app',
-  appName: 'CafeManager',
+  appName: 'SmartCafe',
   webDir: 'dist'
 };
 
